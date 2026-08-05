24eaf1f4e1890f99f5e10468327703667a0cb4ea
breakfastset <34071068+breakfastset@users.noreply.github.com>
2026-08-05 14:49:08 +0800

    Lecture 01

---
+++ lecture_01/another_example.py
@@ -0,0 +1,16 @@
+# different types (Basic types)
+print(type("Hello World"))   # str
+
+print(type(100))    # int
+
+print(type(12.45))   # float
+
+print(type(True))    # bool
+
+print()
+print(ord('A'))     # 65   ordinal value of 'A'
+print(ord('a'))     # 97
+print(chr(119))     # w    character representation of 119
+
+# ASCII
+# Unicode
---
+++ lecture_01/hello_world.py
@@ -0,0 +1,32 @@
+print("Hello,,,, World")
+# this is a comment
+# comments are not interpreted
+# comments are for documentation
+
+# Python is
+# 1. case-sensitive
+# 2. space sensitive
+print("One")   # default print() will end with newline
+print("Two", end="++++")
+print("Three", end="")   # end is not a newline
+print("Four")
+
+print()
+
+# She says "I'll be here tomorrow."
+print("She says \"I'll be here tomorrow.\" ")
+# \" means to escape the " (double quote)
+#    to print the character " instead of interpreting
+#    it as the end of the string
+#
+# special characters:
+# \"   - escape double quote
+# \'   - escape single quote
+# \n   - new line
+# \t   - tab
+# \\   - escape backslash
+# \b   - backspace
+
+
+
+
