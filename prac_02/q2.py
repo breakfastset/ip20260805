0b732d8cbc6869102db2ffeef5c8204edeea0a69
breakfastset <34071068+breakfastset@users.noreply.github.com>
2026-08-05 15:23:51 +0800

    Add Practical 02

---
+++ prac_02/q2.py
@@ -0,0 +1,19 @@
+# Value a is <class 'int'>
+# Value b is <class 'int'>
+# 13 + 5 = 18
+# 13 - 5 = 8
+# 13 * 5 = 65
+# 13 / 5 = 2.6
+# 13 % 5 = 3
+# 13 ** 5 = 371293
+a = 13
+b = 5
+
+print(f"Value a is {type(a)}")
+print(f"Value b is {type(b)}")
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} * {b} = {a*b}")
+print(f"{a} / {b} = {a/b}")
+print(f"{a} % {b} = {a%b}")
+print(f"{a} ** {b} = {a**b}")
